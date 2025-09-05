a0155b33daa50f4dec5cb3fd959ab03055ccbb66
sabiyou <luciusemuh@gmail.com>
2025-09-05 20:47:24 +0100

    Updated package.json

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
 import type { NextConfig } from "next";
 import withFlowbiteReact from "flowbite-react/plugin/nextjs";
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+
 
 const nextConfig: NextConfig = {
   /* config options here */
  
 };
 
-
-initOpenNextCloudflareForDev();
 export default withFlowbiteReact(nextConfig);
