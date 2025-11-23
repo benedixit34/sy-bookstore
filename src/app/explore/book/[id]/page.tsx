f8b124aeda509620dce4e1296361e86f8601eb2a
sabiyou <luciusemuh@gmail.com>
2025-11-24 00:30:49 +0100

    Update Explore Page v2

--- src/app/explore/book/[id]/page.tsx
+++ src/app/explore/book/[id]/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { FooterBottom } from "@/components/Footer";
-import { NavBar } from "@/components/NavBar";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useToast } from "@/hooks/useToast"
+import { useQuery } from "@tanstack/react-query";
 import { useParams } from "next/navigation";
 import { PrimaryButton } from "@/components/ui/WebButton";
 import { handleSave } from "@/utils/handleSave";
@@ -12,41 +12,35 @@ import { BookProps } from "@/lib/types/components";
 export default function Page() {
   const params = useParams();
   const id = params?.id as string;
+  const { toastMessage, showToast } = useToast();
 
-  const [book, setBook] = useState<BookProps | null>(null);
 
-  useEffect(() => {
-    const getBook = async () => {
-      const res = await fetch(`/api/books/${id}`, {
-        cache: "no-store",
-      });
-      if (!res.ok) return;
-      const data = await res.json();
-      setBook(data.book);
-    };
-    getBook();
-  }, [id]);
+  const { data: book, isLoading, isError } = useQuery({
+  queryKey: ["book", id],
+  queryFn: async () => {
+    const res = await fetch(`/api/books/${id}`, { cache: "no-store" });
+    if (!res.ok) throw new Error("Failed to fetch book");
+    const data = await res.json();
+    return data.book;
+  },
+  enabled: !!id,
+});
 
-  const item = { bookId: book?.id ?? "", bookName: book?.name ?? "", imgSrc: book?.image ?? "" };
+  const item = { bookId: book?.id ?? "", bookName: book?.name ?? "", imgSrc: book?.image ?? "", price: book?.price ?? ""};
   
-  const handleSaveWrapper = () => {
-    handleSave(item);
-  };
-  if (!book) {
+  if (isLoading) {
     return (
       <>
-        <NavBar />
         <div className="flex items-center justify-center h-screen">
           <p className="text-gray-500">Loading book...</p>
         </div>
-        <FooterBottom />
       </>
     );
   }
 
   return (
     <>
-      <NavBar />
+     
       <section className="grid grid-cols-1 lg:grid-cols-2 xl:px-32 xl:pt-64 xl:pb-32 md:px-10 py-32 px-4 gap-12 place-items-center">
         <div className="">
           <Image
@@ -65,10 +59,9 @@ export default function Page() {
           <h5 className="text-3xl font-medium font-[lexend]">${book.price}</h5>
           <p className="text-md leading-loose font-[lexend]">{book.description} </p>
 
-          <PrimaryButton hyperlink="/cart" text="Add to Cart" action={() => handleSaveWrapper()} />
+          <PrimaryButton hyperlink="/cart" text="Add to Cart" action={() => handleSave(item, showToast)} />
         </div>
       </section>
-      <FooterBottom />
     </>
   );
 }
